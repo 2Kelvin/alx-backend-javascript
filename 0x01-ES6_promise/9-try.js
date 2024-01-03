688b345e6d84f971d325afc4ded7f96389c35429
2Kelvin <rocketmancodes02@gmail.com>
2024-01-04 01:30:10 +0300

    trial #2

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,16 +1,14 @@
-// Throw an error
-
 export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    let value = mathFunction();
-    queue.push(value);
-    queue.push('Guardrail was processed');
-  }
-  catch (err)  {
-    queue.push(String(err));
-    queue.push('Guardrail was processed');
-  };
+    const queue = [];
+    try {
+        let value = mathFunction();
+        queue.push(value);
+        queue.push('Guardrail was processed');
+    }
+    catch (err) {
+        queue.push(String(err));
+        queue.push('Guardrail was processed');
+    };
 
-  return queue;
+    return queue;
 }
