3171c5e9302f723efc266f76048dc9cfa55f8e2b
2Kelvin <rocketmancodes02@gmail.com>
2024-01-04 01:24:39 +0300

    add all mandatory tasks

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    let value = mathFunction();
+    queue.push(value);
+    queue.push('Guardrail was processed');
+  }
+  catch (err)  {
+    queue.push(String(err));
+    queue.push('Guardrail was processed');
+  };
+
+  return queue;
+}
