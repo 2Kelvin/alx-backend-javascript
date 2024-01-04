a462c6142a759b21777813db7cddf36a1ebeb48a
2Kelvin <rocketmancodes02@gmail.com>
2024-01-05 00:16:23 +0300

    add task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,5 +1,25 @@
+/* eslint-disable */
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor(amount, currency) {}
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() { return this._amount; }
+
+  get currency() { return this._currency; }
+
+  set amount(newValue) { this._amount = newValue; }
+
+  set currency(newValue) { this._currency = newValue; }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
