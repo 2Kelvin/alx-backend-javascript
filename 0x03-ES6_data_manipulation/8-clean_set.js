023220e459cfea5fc3707de104fe5440ab9999be
2Kelvin <rocketmancodes02@gmail.com>
2024-01-10 00:42:16 +0300

    fixes for task 8 #2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(paramSet, startString) {
     return '';
   }
 
-  const matchingWordsArr = Array.from(paramSet).filter((word) => word.startsWith(startString));
+  const matchingWordsArr = [...paramSet].filter((word) => word.startsWith(startString));
 
   return matchingWordsArr.map((eachword) => eachword.slice(startString.length)).join('-');
 }
