13a23993f81b1e958ade085bff9ec4c60aee800a
2Kelvin <rocketmancodes02@gmail.com>
2024-01-10 00:10:37 +0300

    add arg checks

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) throw new Error('Position outside range');
+
   const buffer = new ArrayBuffer(length);
 
   const int8typeArray = new Uint8Array(buffer);
+  int8typeArray[position] = value;
+  const mydataView = new DataView(buffer);
 
-  /* eslint-disable */
-  try {
-    int8typeArray[position] = value;
-    const mydataView = new DataView(buffer);
-
-    return mydataView;
-  } catch (error) {
-    throw error;
-  }
+  return mydataView;
 }
