6a35c59d68f47505dd789910e39055512e5317b6
2Kelvin <rocketmancodes02@gmail.com>
2024-01-10 00:25:08 +0300

    tweak task 100 & add checks for task 8

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpointParam) {
-  let callCount;
+  let callCount = weakMap.get(endpointParam);
 
-  if (weakMap.has(endpointParam)) callCount = weakMap.get(endpointParam);
-  else callCount = 0;
+  if (callCount === undefined) callCount = 0;
+
+  if (callCount >= 5) throw new Error('Endpoint load is high');
 
   weakMap.set(endpointParam, (callCount + 1));
-  if (callCount >= 5) throw new Error('Endpoint load is high');
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(paramSet, startString) {
-  if (!startString || startString.length === 0) return '';
+  if (!startString || startString.length === 0 || !(paramSet instanceof Set)) {
+    return '';
+  }
 
   const matchingWordsArr = Array.from(paramSet).filter((word) => word.startsWith(startString));
 
