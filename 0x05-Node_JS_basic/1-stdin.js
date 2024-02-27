c75cd759a3ae6c105ebb95f931c7c18ed5b08a4e
2Kelvin <rocketmancodes02@gmail.com>
2024-02-28 00:46:57 +0300

    fix task 1 #2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,27 @@
 // importing spawn to create a child process
 const { spawn } = require('node:child_process');
-const readline = require('node:readline');
 
 // message to be echoed & printed when readline closes
 // child process spawned will 'echo' the string provided (like in shell)
 const childProcessEcho = spawn('echo', ['This important software is now closing']);
 
-const rl = readline.createInterface({
+const readline = require('node:readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question(
+// importing displayMessage to console log output
+const displayMessage = require('./0-console');
+
+readline.question(
   'Welcome to Holberton School, what is your name?\n', (usrName) => {
     // console logging the response with the user name given
-    console.log(`Your name is: ${usrName}`);
-    rl.close();
+    displayMessage(`Your name is: ${usrName}`);
+    readline.close();
 
     // displaying child process message to console
     // closeMsg is in buffer (bytes) format ...
     // ... so you have to convert it to a string first before printing it
-    childProcessEcho.stdout.on('data', (closeMsg) => console.log(closeMsg.toString()));
+    childProcessEcho.stdout.on('data', (closeMsg) => displayMessage(closeMsg.toString()));
   },
 );
