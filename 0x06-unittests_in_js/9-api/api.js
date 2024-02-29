40afc7a6ea9f4aecf02f1daebb6285483e94f49b
2Kelvin <rocketmancodes02@gmail.com>
2024-02-29 16:28:40 +0300

    add task 9#2

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-app.listen(7865, () => console.log('API available on localhost port 7865'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
@@ -11,5 +10,7 @@ app.get('/', (req, res) => {
 app.get('/cart/:id(\\d+)', (res, req) => {
   res.send(`Payment methods for cart: ${req.params.id}`);
 });
+
+app.listen(7865, () => console.log('API available on localhost port 7865'));
 
 module.exports = app;
