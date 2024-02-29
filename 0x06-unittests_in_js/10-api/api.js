2e5fef950f410f33e381c66d786dae7268b803e0
2Kelvin <rocketmancodes02@gmail.com>
2024-02-29 18:49:54 +0300

    add task 10#4

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -25,13 +25,12 @@ app.get('/available_payments', (req, res) => {
 
 // post login route
 app.post('/login', (req, res) => {
-  let username = '';
-
-  if (req.body) {
-    username = req.body.userName;
+  const userName = req.body.userName;
+  if (userName) {
+    res.send(`Welcome ${userName}`);
+  } else {
+    res.send(`Welcome`);
   }
-
-  res.send(`Welcome ${username}`);
 });
 
 app.listen(7865, () => console.log('API available on localhost port 7865'));
