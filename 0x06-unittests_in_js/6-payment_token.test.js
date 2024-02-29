1ffee52328c550b43f32d02213c2820bac79925e
2Kelvin <rocketmancodes02@gmail.com>
2024-02-29 15:46:41 +0300

    add task 6#4

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,17 +1,12 @@
-/* eslint-disable jest/expect-expect */
-/* eslint-disable jest/valid-expect */
 const { expect } = require('chai');
-const assert = require('assert');
-
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('returns a resolved data promise', () => new Promise((done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({data: 'Successful response from the API' });
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
         done();
-      })
-      .catch(done);
-  }));
+      });
+  });
 });
