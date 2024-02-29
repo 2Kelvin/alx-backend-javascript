0542fb14cbab4e27f65d526b3d108764cc4c37d4
2Kelvin <rocketmancodes02@gmail.com>
2024-02-29 16:10:34 +0300

    add task 8#2

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 describe('api test', () => {
   const url = 'http://localhost:7865';
 
-  it('GET / returns correct response', (done) => {
+  it('returns route text', (done) => {
     request.get(`${url}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
