22c7cde7191be2e9c70d64c1eaa97628b3351242
2Kelvin <rocketmancodes02@gmail.com>
2024-02-29 14:19:38 +0300

    add task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,41 +3,40 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-// You can assume a and b are always number
-// Tests should be around the “rounded” part
+// tests
 
 describe('calculateNumber', () => {
-  it('handles floating point whole numbers', () => {
+  it('Number point zero', () => {
     assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
-  it('handles b\'s floating point fractional number', () => {
+  it('mixed numbers', () => {
     assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
-  it('handles a and b\'s floating point fractional number', () => {
+  it('both floats', () => {
     assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
-  it('handles a\'s floating point fractional number', () => {
+  it('a is float', () => {
     assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
-  it('handles negative numbers', () => {
+  it('a negative', () => {
     assert.equal(calculateNumber(-1.2, 3.8), 3);
   });
-  it('handles b\'s floating point fractional numbers', () => {
+  it('b float', () => {
     assert.strictEqual(calculateNumber(1.0, 2.5), 4);
   });
-  it('handles zero', () => {
+  it('zero', () => {
     assert.equal(calculateNumber(0, 0), 0);
   });
-  it('handles floating point fractional numbers', () => {
+  it('almost same', () => {
     assert.strictEqual(calculateNumber(2.6, 2.5), 6);
   });
-  it('handles large numbers', () => {
+  it('large a and b', () => {
     assert.equal(calculateNumber(1000000000.2, 3000000000.8), 4000000001);
   });
-  it('handles floating point fractional numbers with trailing 9\'s', () => {
+  it('many floats', () => {
     assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
-  it('handles edge cases', () => {
+  it('other cases', () => {
     assert.equal(calculateNumber(0.4, 0.6), 1);
     assert.equal(calculateNumber(0.49, 0.51), 1);
     assert.equal(calculateNumber(-0.4, -0.6), -1);
